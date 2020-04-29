7c49e02c5d7310fb6e48aa99a4492fd909b77e9b
pcweb <contact@pcweb-conseils.com>
2020-04-29 12:46:03 +0200

    modif nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,16 +2,9 @@
   <header>
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" id="navId">
-      <nuxt-link to="/">
-        <li class="nav-item">
-          <a href="#" class="nav-link disabled">Home</a>
-        </li></nuxt-link
-      >
-      <nuxt-link to="about">
-        <li class="nav-item">
-          <a href="#tab5Id" class="nav-link">A propos</a>
-        </li></nuxt-link
-      >
+      <nuxt-link to="/">Home</nuxt-link>
+
+      <nuxt-link to="/about">A propos</nuxt-link>
     </ul>
 
     <!-- Tab panes -->
