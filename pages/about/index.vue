4caace553a4dc423b6f05a3445ac7f8629dd8803
pcweb <contact@pcweb-conseils.com>
2020-04-29 12:23:27 +0200

    about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,15 +1,22 @@
 <template class="container-fluid">
   <section class="row justify-content-around">
-    <article class="col-md-6 bg-warning article">
+    <article class="col-md-6 article">
       <h2>A propos de moi</h2>
       <p>Mon parcours avec sa description.</p>
     </article>
-    <aside class="col-md-2 bg-primary">
-      <h2>Mes envies du moment</h2>
+    <aside class="col-md-2">
+      <h2>Mes lectures du moment</h2>
       <p>La liste de mes envies du moment</p>
     </aside>
   </section>
 </template>
 <style>
-
+article{
+  text-align: center;
+  border:  2px solid #000000;
+  border-radius: 50%;
+  width: 450px;
+  height: 450px;
+  max-width: 300px;
+}
 </style>
